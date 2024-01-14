3ed69c6b9b5504ed7817930bfa3fff8ea812baf3
Saral Sainju <sainju.saral433@gmail.com>
2024-01-14 18:24:24 +0545

    commit second

--- SolarSystem.py
+++ SolarSystem.py
@@ -6,8 +6,6 @@ import time
 import math
 import random
 
-# latest version in this file
-
 start_time = time.time()
 
 year = 0
@@ -54,20 +52,20 @@ def Orbit(radius, x, y, z):
 def Mercury(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodM = 0.24 * 8  # 0.24 * 8 seconds for simulating one year
+    year_periodM = 0.24 * 8
     yearM = t / year_periodM
     dayM = 88 * yearM
 
-    glRotatef(yearM * 360.0, 0, 1.0, 0.0)  # Mercury rotation around the sun
-    Orbit(1.2, 0, -0.25, 0.0)  # radius 1.2 times 0.4 * 2.5
+    glRotatef(yearM * 360.0, 0, 1.0, 0.0)
+    Orbit(1.2, 0, -0.25, 0.0)
 
-    glTranslatef(1.2, -0.25, 0.0)  # Mercury location
-    glPushMatrix()  # push Venus system
-    glPushMatrix()  # push Mercury system
-    glRotatef(dayM * 360, 0.0, 0.5, 0.0)  # Mercury spinn
-    glRotatef(90, 1.0, 3.0, 0.0)  # Mercury axis
+    glTranslatef(1.2, -0.25, 0.0)
+    glPushMatrix()
+    glPushMatrix()
+    glRotatef(dayM * 360, 0.0, 0.5, 0.0)
+    glRotatef(90, 1.0, 3.0, 0.0)
     glColor3f(0.5, 0.5, 0.5)
-    glutSolidSphere(0.05, 10, 8)  # Mercury
+    glutSolidSphere(0.05, 10, 8)
     glPopMatrix()
     glPopMatrix()
 
@@ -75,45 +73,45 @@ def Mercury(t):
 def Venus(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodV = 0.62 * 8  # 0.62 * 8 seconds for simulating one year
+    year_periodV = 0.62 * 8
     yearV = t / year_periodV
     dayV = 225 * yearV
-    glRotatef(yearV * 360.0, 0, 1, 0)  # Venus rotation around the sun
-    Orbit(1.5, 0, -0.25, 0.0)  # radius 1.2 times 0.5 * 2.5
+    glRotatef(yearV * 360.0, 0, 1, 0)
+    Orbit(1.5, 0, -0.25, 0.0)
 
-    glTranslatef(1.5, -0.25, 0.0)  # Venus location
-    glPushMatrix()  # push Venus system
+    glTranslatef(1.5, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(dayV * 360, 0.0, 1.0, 0.0)  # Venus spinn
+    glPushMatrix()
+    glRotatef(dayV * 360, 0.0, 1.0, 0.0)
     glRotatef(90, 3.0, 2.0, 0.0)
     glColor3f(1, 0.5, 0)
-    glutSolidSphere(0.1, 10, 8)  # Venus
+    glutSolidSphere(0.1, 10, 8)
     glPopMatrix()
     glPopMatrix()
 
 
 def Earth_System(t):
-    year_period = 1 * 8  # 1 * 8 seconds for simulating one year
+    year_period = 1 * 8
     year = t / year_period
     day = 365 * year
     moon_syn = (365 / 29.5) * year
-    glRotatef(year * 360.0, 0, 1, 0)  # earth rotation around the sun
-    Orbit(2.1, 0, -0.25, 0.0)  # radius 1.2 times 0.7 * 2.5
+    glRotatef(year * 360.0, 0, 1, 0)
+    Orbit(2.1, 0, -0.25, 0.0)
 
-    glTranslatef(2.1, -0.25, 0.0)  # earth location
-    glPushMatrix()  # push earth system
+    glTranslatef(2.1, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(day * 360.0, 0.0, 1.0, 0.0)  # earth spinn
-    glRotatef(90 - 23.4, 1.0, 1.0, 0.0)  # earth axis
+    glPushMatrix()
+    glRotatef(day * 360.0, 0.0, 1.0, 0.0)
+    glRotatef(90 - 23.4, 1.0, 1.0, 0.0)
     glColor3f(0.1, 0.4, 1)
-    glutSolidSphere(0.15, 10, 8)  # earth
+    glutSolidSphere(0.15, 10, 8)
     glPopMatrix()
     glPushMatrix()
-    glRotatef(moon_syn * 360.0, 0.0, 1.0, 0.0)  # moon synodic
-    glTranslatef(0.5, 0.0, 0.0)  # distance moon to earth
+    glRotatef(moon_syn * 360.0, 0.0, 1.0, 0.0)
+    glTranslatef(0.5, 0.0, 0.0)
     glRotatef(90, 1.0, 1.0, 0.0)
     glColor4f(0.4, 0.5, 0.6, 1)
-    glutSolidSphere(0.05, 10, 8)  # moon
+    glutSolidSphere(0.05, 10, 8)
     glPopMatrix()
     glPopMatrix()
 
@@ -121,19 +119,19 @@ def Earth_System(t):
 def Mars(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodMa = 1.88 * 8  # 1.88 * 8 seconds for simulating one year
+    year_periodMa = 1.88 * 8
     yearMa = t / year_periodMa
     dayMa = 687 * yearMa
-    glRotatef(yearMa * 360.0, 0, 1, 0)  # Mars rotation around the sun
-    Orbit(2.7, 0, -0.25, 0.0)  # radius 1.2 times 0.9 * 2.5
+    glRotatef(yearMa * 360.0, 0, 1, 0)
+    Orbit(2.7, 0, -0.25, 0.0)
 
-    glTranslatef(2.7, -0.25, 0.0)  # Mars location
-    glPushMatrix()  # push Mars system
+    glTranslatef(2.7, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(dayMa * 360, 0.0, 1.0, 0.0)  # Mars spinn
-    glRotatef(75, 3.0, 2.0, 1.0)  # Mars axis
+    glPushMatrix()
+    glRotatef(dayMa * 360, 0.0, 1.0, 0.0)
+    glRotatef(75, 3.0, 2.0, 1.0)
     glColor3f(1, 0.3, 0)
-    glutSolidSphere(0.12, 10, 8)  # Mars
+    glutSolidSphere(0.12, 10, 8)
     glPopMatrix()
     glPopMatrix()
 
@@ -141,19 +139,18 @@ def Mars(t):
 def Jupiter(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodJ = 11.86 * 8  # 11.86 * 8 seconds for simulating one year
+    year_periodJ = 11.86 * 8
     yearJ = t / year_periodJ
     dayJ = 4332 * yearJ
-    glRotatef(yearJ * 360.0, 0, 1, 0)  # Jupiter rotation around the sun
-    Orbit(3.6, 0, -0.25, 0.0)  # radius 1.2 times 1.2 * 2.5
+    glRotatef(yearJ * 360.0, 0, 1, 0)
+    Orbit(3.6, 0, -0.25, 0.0)
 
-    glTranslatef(3.6, -0.25, 0.0)  # Jupiter location
-    glPushMatrix()  # push Jupiter system
+    glTranslatef(3.6, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(dayJ * 360, 0.0, 1.0, 0.0)  # Jupiter spinn
-    glRotatef(75, 3.0, 2.0, 0.0)  # Jupiter axis
-    glColor3f(1, 0.6, 0.1)
-    glutSolidSphere(0.4, 20, 16)  # Jupiter
+    glPushMatrix()
+    glRotatef(dayJ * 360, 0.0, 1.0, 0.0)
+    glRotatef(75, 3.0, 2.0, 0.0)
+    glutSolidSphere(0.4, 20, 16)
     glPopMatrix()
     glPopMatrix()
 
@@ -161,17 +158,17 @@ def Jupiter(t):
 def Saturn(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodS = 29.46 * 8  # 29.46 * 8 seconds for simulating one year
+    year_periodS = 29.46 * 8
     yearS = t / year_periodS
-    glRotatef(yearS * 360.0, 0, 1, 0)  # Saturn rotation around the sun
-    Orbit(4.5, 0, -0.25, 0.0)  # radius 1.2 times 1.5 * 2.5
-    glTranslatef(4.5, -0.25, 0.0)  # Saturn location
-    glPushMatrix()  # push Saturn system
+    glRotatef(yearS * 360.0, 0, 1, 0)
+    Orbit(4.5, 0, -0.25, 0.0)
+    glTranslatef(4.5, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(170, 1.0, 2.0, 4.0)  # Saturn axis
+    glPushMatrix()
+    glRotatef(170, 1.0, 2.0, 4.0)
     glColor3f(1, 0.8, 0.4)
     gluDisk(gluNewQuadric(), 0.4, 0.5, 60, 4)
-    glutSolidSphere(0.3, 20, 16)  # Saturn
+    glutSolidSphere(0.3, 20, 16)
     glPopMatrix()
     glPopMatrix()
 
@@ -179,19 +176,19 @@ def Saturn(t):
 def Uranus(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodU = 84.02 * 8  # 84.02 * 8 seconds for simulating one year
+    year_periodU = 84.02 * 8
     yearU = t / year_periodU
     dayU = 687 * yearU
-    glRotatef(yearU * 360.0, 0, 1, 0)  # Uranus rotation around the sun
-    Orbit(5.4, 0, -0.25, 0.0)  # radius 1.2 times 1.8 * 2.5
+    glRotatef(yearU * 360.0, 0, 1, 0)
+    Orbit(5.4, 0, -0.25, 0.0)
 
-    glTranslatef(5.4, -0.25, 0.0)  # Uranus location
-    glPushMatrix()  # push Uranus system
+    glTranslatef(5.4, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(dayU * 360, 0.0, 1.0, 0.0)  # Uranus spinn
-    glRotatef(50, 1, 2.0, 1)  # Uranus axis
+    glPushMatrix()
+    glRotatef(dayU * 360, 0.0, 1.0, 0.0)
+    glRotatef(50, 1, 2.0, 1)
     glColor3f(0.4, 1, 0.8)
-    glutSolidSphere(0.2, 10, 8)  # Uranus
+    glutSolidSphere(0.2, 10, 8)
     glPopMatrix()
     glPopMatrix()
 
@@ -199,19 +196,19 @@ def Uranus(t):
 def Neptune(t):
     glPopMatrix()
     glPushMatrix()
-    year_periodN = 164.79 * 8  # 164.79 * 8 seconds for simulating one year
+    year_periodN = 164.79 * 8
     yearN = t / year_periodN
     dayN = 687 * yearN
-    glRotatef(yearN * 360.0, 0, 1, 0)  # Neptune rotation around the sun
-    Orbit(6, 0, -0.25, 0.0)  # radius 1.2 times 2 * 2.5
+    glRotatef(yearN * 360.0, 0, 1, 0)
+    Orbit(6, 0, -0.25, 0.0)
 
-    glTranslatef(6, -0.25, 0.0)  # Neptune location
-    glPushMatrix()  # push Neptune system
+    glTranslatef(6, -0.25, 0.0)
     glPushMatrix()
-    glRotatef(dayN * 360, 0.0, 1.0, 0.0)  # Neptune spinn
-    glRotatef(70, 1, 2.0, 1)  # Neptune axis
+    glPushMatrix()
+    glRotatef(dayN * 360, 0.0, 1.0, 0.0)
+    glRotatef(70, 1, 2.0, 1)
     glColor3f(0.1, 0.9, 1)
-    glutSolidSphere(0.2, 20, 40)  # Neptune
+    glutSolidSphere(0.2, 20, 40)
     glPopMatrix()
     glPopMatrix()
 
@@ -244,7 +241,6 @@ def Reshape(w, h):
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     gluLookAt(0.0, 2.0, 5.0, 0.0, -3.5, 0.0, 0.0, 2.0, 0.0)
-    # gluLookAt(0.0, 2.0, 5.0, x, y, z, 0.0, 2.0, 0.0)
 
 
 def main():
